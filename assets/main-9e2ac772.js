4756dd51c5f84d78fc9774ef297a2cd56d061d55
Sokiryanin <105035206+Sokiryanin@users.noreply.github.com>
2024-09-30 13:10:52 +0000

    Deploying to gh-pages from @ Sokiryanin/home-service@7c23fc52f6e42088ed81064aee180c04c58e37c9 🚀

--- assets/main-9e2ac772.js
+++ assets/main-9e2ac772.js
@@ -14,4 +14,4 @@ import{S as E,N as k,P as q}from"./vendor-f275e1b7.js";(function(){const t=docum
               ${e}
           </div>
       </article>`;t.innerHTML+=o})}async function j(){try{const i=await fetch("data/cards.json");if(!i.ok)throw new Error("Ошибка загрузки данных");return(await i.json()).items}catch(i){return console.error("Error:",i),[]}}M();window.FLS=!0;T();C();O();P();
-//# sourceMappingURL=main-a4e0de68.js.map
+//# sourceMappingURL=main-9e2ac772.js.map
